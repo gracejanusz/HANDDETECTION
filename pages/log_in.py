ef2d9107a14d6f1c27943a9d661cd8dd0b002da5
Elena Loucks <elenaloucks@Elenas-MacBook-Pro.local>
2025-04-26 16:20:36 -0700

    gah

--- pages/log_in.py
+++ pages/log_in.py
@@ -22,7 +22,7 @@ firebase_credentials = {
     "type": st.secrets["firebase"]["type"],
     "project_id": st.secrets["firebase"]["project_id"],
     "private_key_id": st.secrets["firebase"]["private_key_id"],
-    "private_key": st.secrets["firebase"]["private_key"],
+    "private_key": st.secrets["firebase"]["private_key"].replace('\\n', '\n'),
     "client_email": st.secrets["firebase"]["client_email"],
     "client_id": st.secrets["firebase"]["client_id"],
     "auth_uri": st.secrets["firebase"]["auth_uri"],
